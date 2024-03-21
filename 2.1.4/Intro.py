206f1fc7253401a6284b7b5f484404053073edaf
Ryan <ryan.ajamian@vtsd.com>
2024-03-21 07:58:11 -0400

    a

--- 2.1.4/Intro.py
+++ 2.1.4/Intro.py
@@ -4,7 +4,7 @@ import tkinter as tk
 
 # main window
 root = tk.Tk()
-root.wm_geometry("500x500")
+root.wm_geometry("150x150")
 
 # create empty frame
 frame_login = tk.Frame(root)
@@ -17,9 +17,14 @@ ent_username = tk.Entry(frame_login, bd=3)
 ent_username.pack(pady=5)
 
 lbl_password = tk.Label(frame_login, text='Password:')
-lbl_password.pack()
+lbl_password.pack(padx=50)
 
 ent_password = tk.Entry(frame_login, bd=3)
 ent_password.pack(pady=5)
+
+bt_login = tk.Button(frame_login, text='Login')
+bt_login.pack()
+
+
 
 root.mainloop()
