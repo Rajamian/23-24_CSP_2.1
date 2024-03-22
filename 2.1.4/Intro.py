d74a2b14433c7b8f74a7e2a0fa362d68c9380dc0
Ryan <ryan.ajamian@vtsd.com>
2024-03-22 09:40:37 -0400

    a

--- 2.1.4/Intro.py
+++ 2.1.4/Intro.py
@@ -2,6 +2,12 @@
 #   A program creates a window on your screen using Tkinter.
 import tkinter as tk
 
+def test_my_botton():
+    if(ent_username.get() == "username" and ent_password.get() == "pass"):
+        frame_auth.tkraise()
+    else:
+        fail_label = tk.Label(frame_login, text="Invalid user/pass combination")
+        fail_label.pack()
 # main window
 root = tk.Tk()
 root.wm_geometry("150x150")
@@ -9,6 +15,9 @@ root.wm_geometry("150x150")
 # create empty frame
 frame_login = tk.Frame(root)
 frame_login.grid()
+
+frame_auth =tk.Frame(root)
+frame_auth.grid()
 
 lbl_username = tk.Label(frame_login, text='Username:')
 lbl_username.pack()
@@ -22,7 +31,7 @@ lbl_password.pack(padx=50)
 ent_password = tk.Entry(frame_login, bd=3)
 ent_password.pack(pady=5)
 
-bt_login = tk.Button(frame_login, text='Login')
+bt_login = tk.Button(frame_login, text='Login',command=test_my_botton)
 bt_login.pack()
 
 
